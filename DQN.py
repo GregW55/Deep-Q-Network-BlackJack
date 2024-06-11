103b35eb8b6da21c5d40c3940fc22ba70afbb7d5
GregW55 <163344650+GregW55@users.noreply.github.com>
2024-06-11 19:08:47 -0400

    Update DQN.py

--- DQN.py
+++ DQN.py
@@ -16,7 +16,7 @@ device = torch.device("cuda")
 
 learning_rate = 0.001
 input_size = 4  # Player_value, Dealer_showing_value, Player_usable_ace, dealer_usable_ace
-hidden_size = 32
+hidden_size = 4
 output_size = 2  # 0:Hit 1:Stay
 episodes = 10_000
 eval_episodes = 10000
